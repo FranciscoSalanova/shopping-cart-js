dd6a2aa2a2b46b27f8c77c061f6ee48fe521cb48
Fran Salanova <fsalanova.16@gmail.com>
2024-01-27 20:02:31 -0300

    Finishing the main product

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,7 +1,11 @@
 import formatCurrency from "./util/formatCurrency.js"
+import addGlobalEventListener from "./util/addGlobalEventListener.js"
 import items from "./items.json"
 
 const cartButton = document.querySelector("[data-cart-button]")
+const removeFromCartButton = document.querySelector(
+  "[data-remove-from-cart-button]"
+)
 const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper]")
 const cartItemTemplate = document.querySelector("#cart-item-template")
 const cartItemContainer = document.querySelector("[data-cart-items]")
@@ -10,17 +14,34 @@ const cartTotal = document.querySelector("[data-cart-total]")
 const cart = document.querySelector("[data-cart]")
 
 const IMAGE_URL = "https://dummyimage.com/210x130"
+const SESSION_STORAGE_KEY = "SHOPPING_CART-cart"
 let shoppingCart = []
 
 export function setupShoppingCart() {
+  addGlobalEventListener("click", "[data-remove-from-cart-button]", (e) => {
+    const id = parseInt(e.target.closest("[data-item]").dataset.itemId)
+
+    removeFromCart(id)
+  })
+
+  shoppingCart = loadCart()
+
+  cartButton.addEventListener("click", () => {
+    cartItemsWrapper.classList.toggle("invisible")
+  })
+
   renderCart()
 }
 
 // Persist the cart accross multiple pages
+function saveCart() {
+  sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(shoppingCart))
+}
 
-cartButton.addEventListener("click", () => {
-  cartItemsWrapper.classList.toggle("invisible")
-})
+function loadCart() {
+  const cart = sessionStorage.getItem(SESSION_STORAGE_KEY)
+  return JSON.parse(cart) || []
+}
 
 function renderCart() {
   if (shoppingCart.length === 0) {
@@ -49,9 +70,17 @@ export function addToCart(id) {
   }
 
   renderCart()
+  saveCart()
 }
 
-// Remove items from cart
+function removeFromCart(id) {
+  const existingItem = shoppingCart.find((entry) => entry.id === id)
+  if (existingItem == null) return
+  shoppingCart = shoppingCart.filter((entry) => entry.id !== id)
+
+  renderCart()
+  saveCart()
+}
 
 function renderCartItems() {
   cartQuantity.innerHTML = shoppingCart.length
--- store.js
+++ store.js
@@ -1,19 +1,20 @@
 import items from "./items.json"
 import { addToCart } from "./shoppingCart.js"
 import formatCurrency from "./util/formatCurrency.js"
+import addGlobalEventListener from "./util/addGlobalEventListener.js"
 
 const storeItemTemplate = document.querySelector("#store-item-template")
-const storeItemContainer = document.querySelector("[data-store-container]")
+const storeContainer = document.querySelector("[data-store-container]")
 const IMAGE_URL = "https://dummyimage.com/420x260"
 
 /** Función para controlar la configuración y visualización de los items de la página */
 export function setupStore() {
-  document.addEventListener("click", (e) => {
-    if (e.target.matches("[data-add-to-cart-button]")) {
-      const id = e.target.closest("[data-store-item]").dataset.itemId
+  if (storeContainer == null) return
 
-      addToCart(parseInt(id))
-    }
+  addGlobalEventListener("click", "[data-add-to-cart-button]", (e) => {
+    const id = e.target.closest("[data-store-item]").dataset.itemId
+
+    addToCart(parseInt(id))
   })
 
   items.forEach(renderStoreItem)
@@ -33,5 +34,5 @@ function renderStoreItem(item) {
   const image = storeItem.querySelector("[data-image]")
   image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
 
-  storeItemContainer.appendChild(storeItem)
+  storeContainer.appendChild(storeItem)
 }
